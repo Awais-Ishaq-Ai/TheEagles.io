03c7e40c6c9e5dd94484ffaafd550455e42a3927
Aqibmalik57 <aqibmalik1586@gmail.com>
2025-01-23 14:27:18 +0500

    setting the arrow icon

--- src/Components/Lvl3/Notify.jsx
+++ src/Components/Lvl3/Notify.jsx
@@ -6,14 +6,16 @@ const Notify = () => {
     <>
       <div className='w-full flex justify-center'>
         <div
-          className='h-[240px] w-full bg-auto bg-no-repeat bg-top'
+          className='h-[240px] w-auto bg-auto bg-no-repeat bg-top'
           style={{ backgroundImage: `url(${bgImage})` }}
         >
-          <GoArrowUpRight className='text-textColor3 mt-5 text-lg ms-auto me-10' />
-          <h1 className='text-lg font-semibold text-textColor3 w-1/2 mx-auto'>
-            Official <span className='text-landingtext'>Notify Bot</span> for
-            busd.The Eagles.io Users
-          </h1>
+          <div className='relative mt-12 w-full'>
+            <GoArrowUpRight className='text-textColor3 mt-5 text-lg ms-auto me-10 absolute -top-7 right-7' />
+            <h1 className='text-lg font-semibold text-textColor3 w-1/2 mx-auto'>
+              Official <span className='text-landingtext'>Notify Bot</span> for
+              busd.The Eagles.io Users
+            </h1>
+          </div>
           <p className='text-textColor3 w-1/2 mt-3 text-xs mx-auto text-center'>
             Here you can get notifications all the event in your account
           </p>
