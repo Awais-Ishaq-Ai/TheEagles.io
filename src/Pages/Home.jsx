b405584b661310d4b560d8ca2b96b3751cd0508e
Aqibmalik57 <aqibmalik1586@gmail.com>
2025-01-29 17:47:17 +0500

    adding the image on home file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,63 +27,73 @@ const Home = () => {
   return (
     <>
       <div className='w-full px-4 pt-8 homebg bg-[#0000ff40]'>
-        <div className='bg-eagles'>
-          <div className='h-auto flex justify-between'>
-            <div className='flex gap-6'>
-              <div className='gradient-border h-20 w-20 rounded-full'>
-                <div className='relative flex items-center justify-center text-white h-full w-full rounded-full bg-Background'>
-                  <p className='text-[#bcbcbc]'>
-                    <IoPersonCircleSharp className='text-7xl text-textColor3' />
+        <div className='relative'>
+          <div className='absolute inset-0 h-full opacity-10 left-12'>
+            <img
+              src='assets/HomeImages/eaglebg.jpg'
+              alt='Background'
+              className='w-full h-full object-cover'
+            />
+          </div>
+          <div className='bg-eagles relative inset-0 z-10'>
+            <div className='h-auto flex justify-between'>
+              <div className='flex gap-6'>
+                <div className='gradient-border h-20 w-20 rounded-full'>
+                  <div className='relative flex items-center justify-center text-white h-full w-full rounded-full bg-Background'>
+                    <p className='text-[#bcbcbc]'>
+                      <IoPersonCircleSharp className='text-7xl text-textColor3' />
+                    </p>
+                    <img
+                      src='/assets/HomeImages/logo.png'
+                      alt='logo'
+                      className='h-7 w-7 ms-2 object-cover absolute bottom-1 -right-4 rounded-full'
+                    />
+                  </div>
+                </div>
+                <div className='text-textColor3'>
+                  <h1 className='text-lg font-semibold font-sans'>Username</h1>
+                  <p className='text-lg text-yellow-300 italic font-medium'>
+                    ID 5436547
                   </p>
-                  <img
-                    src='/assets/HomeImages/logo.png'
-                    alt='logo'
-                    className='h-7 w-7 ms-2 object-cover absolute bottom-1 -right-4 rounded-full'
-                  />
+                  <button
+                    className='ms-4 mt-3 text-base flex gap-2 items-center justify-center bg-Background text-textColor2 w-44 py-1 rounded-full'
+                    onClick={() => setShowDetails(!showDetails)}
+                  >
+                    {showDetails ? 'Show less' : 'Show more'}
+                    <span>
+                      {showDetails ? (
+                        <IoIosArrowUp className='text-base' />
+                      ) : (
+                        <IoIosArrowDown className='text-base' />
+                      )}
+                    </span>
+                  </button>
                 </div>
               </div>
-              <div className='text-textColor3'>
-                <h1 className='text-lg font-semibold font-sans'>Username</h1>
-                <p className='text-lg text-yellow-300 italic font-medium'>
-                  ID 5436547
-                </p>
-                <button
-                  className='ms-4 mt-3 text-base flex gap-2 items-center justify-center bg-Background text-textColor2 w-44 py-1 rounded-full'
-                  onClick={() => setShowDetails(!showDetails)}
-                >
-                  {showDetails ? 'Show less' : 'Show more'}
-                  <span>
-                    {showDetails ? (
-                      <IoIosArrowUp className='text-base' />
-                    ) : (
-                      <IoIosArrowDown className='text-base' />
-                    )}
-                  </span>
-                </button>
+              <div>
+                <IoSettingsSharp
+                  className='text-textColor3 text-3xl mt-1'
+                  onClick={() => navigate('/profile')}
+                />
               </div>
             </div>
-            <div>
-              <IoSettingsSharp
-                className='text-textColor3 text-3xl mt-1'
-                onClick={() => navigate('/profile')}
-              />
-            </div>
+            {showDetails && (
+              <div className='mt-3'>
+                <div className='flex gap-x-2 items-center text-sm text-textColor3'>
+                  <p>0x783c...2sso</p>
+                  <IoCopy />
+                </div>
+                <div className='flex gap-2 items-center text-textColor3 text-sm'>
+                  <p>invited 24.11.2024 by</p>
+                  <p className='px-3 flex justify-center text-yellow-300 font-medium text-lg bg-[#333333] bg-opacity-35  rounded-full italic'>
+                    ID 4677755
+                  </p>
+                </div>
+              </div>
+            )}
           </div>
-          {showDetails && (
-            <div className='mt-3'>
-              <div className='flex gap-x-2 items-center text-sm text-textColor3'>
-                <p>0x783c...2sso</p>
-                <IoCopy />
-              </div>
-              <div className='flex gap-2 items-center text-textColor3 text-sm'>
-                <p>invited 24.11.2024 by</p>
-                <p className='px-3 flex justify-center text-yellow-300 font-medium text-lg bg-[#333333] bg-opacity-35  rounded-full italic'>
-                  ID 4677755
-                </p>
-              </div>
-            </div>
-          )}
         </div>
+
         <div className='bg-textColor bg-opacity-20 w-full px-2 py-3 rounded-lg mt-4'>
           <div className='flex items-center justify-between text-base mb-5'>
             <h5 className='text-textColor3'>Personal link</h5>
